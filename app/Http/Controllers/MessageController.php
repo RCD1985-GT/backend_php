110bd2fb087d0aad1cdde0394c6cf427ba35ad69
Rodrigo <infohotsmoke@gmail.com>
2022-04-07 20:22:58 +0200

    functions added in MessageController.php

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -3,8 +3,108 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Message;
+use App\Models\User;
+use Illuminate\Database\QueryException;
 
 class MessageController extends Controller
 {
-    //
+    public function showAllMessages() {
+
+        try {
+            
+            return Message::all();
+
+        } 
+        
+        catch(QueryException $error) {
+
+            return $error;
+
+        }
+
+    }
+
+    public function añadirMessage(Request $request) {
+
+        $user = $request->input('userId');
+        $partyId = $request->input('partyId');
+        $message = $request->input('body');
+
+        try {
+
+            return Message::create([
+
+                'userID' => $user,
+                'body' => $message,
+                'partyId' => $partyId
+
+            ]);
+
+        } 
+        
+        catch (QueryException $error) {
+
+            $codigoError = $error->errorInfo[1];
+
+            return response()->json([
+
+                'error' => $codigoError
+
+            ]);
+            
+        }
+
+    }
+
+    public function updateMessages(Request $request) {
+
+        $user = User::id();
+        $messageId = $request->input('messageId');
+
+        try { 
+            
+            $msg = ['message'=>$request->message];
+            return Message::where('id', '=', $messageId)->update($msg);
+
+        } 
+        
+        catch (QueryException $error) {
+
+            $codigoError = $error->errorInfo[1];
+
+            return response()->json([
+
+                'error' => $codigoError
+
+            ]);
+            
+        }
+
+    }
+
+    public function deleteMessage(Request $request) {
+
+        $user = User::id();
+        $messageId = $request->input('messageId');
+
+        try {
+
+            return Message::where('id', '=', $messageId)->delete($messageId);
+
+        } 
+        
+        catch (QueryException $error) {
+
+            $codigoError = $error->errorInfo[1];
+
+            return response()->json([
+
+                'error' => $codigoError
+
+            ]);
+            
+        }
+
+    }
 }
