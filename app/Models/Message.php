35bf24bdf9a5729b1b5df84d6ffec072276a0f58
Rodrigo <infohotsmoke@gmail.com>
2022-04-07 20:20:49 +0200

    Message model updated

--- app/Models/Message.php
+++ app/Models/Message.php
@@ -8,4 +8,17 @@ use Illuminate\Database\Eloquent\Model;
 class Message extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'body', 'userID', 'partyId'
+    ];
+
+
+    public function user()
+    {
+        return $this->belongsTo('App\Models\User','userID','Id');
+    }
+    public function party()
+    {
+        return $this->belongsTo('App\Models\Party','partyID','Id');
+    }
 }
