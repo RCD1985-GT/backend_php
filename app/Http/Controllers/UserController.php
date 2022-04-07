5e5beabb77fea1e3140232fe18b11c04ca0bb8f4
Rodrigo <infohotsmoke@gmail.com>
2022-04-07 19:44:18 +0200

    UserController.php created

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,9 +3,123 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Database\QueryException;
+
+
+use App\Models\User;
 
 class UserController extends Controller
 {
-    //
+    public function showAllUsers(){
+
+        try {
+            
+            return User::all();
+
+        } catch(QueryException $error) {
+            return $error;
+        }
+    }
+
+    public function showProfile(Request $request){
+
+        $id = $request->input('id');
+
+        try {
+
+            return User::all()->where('id', '=', $id)
+            ->makeHidden(['password'])->keyBy('id');
+
+        } catch (QueryException $error) {
+            return $error;
+        }
+    }
+    
+
+    public function registerUser(Request $request){
+
+        $email = $request->input('email');
+        $name = $request->input('name');
+        $password = $request->input('password');
+
+        $this -> validate( $request, [
+            'name' => 'required|string',
+            'email' => 'required|email',
+            'password' => 'required|min:8',
+        ]);
+
+        try {
+            
+            return User::create(
+                [
+                    'email' => $email,
+                    'name' => $name,
+                    'password' => $password
+                ]
+                );
+        
+        } catch (QueryException $error) {
+
+            $codigoError = $error->errorInfo[1];
+
+            if($codigoError == 1062) {
+                return response()->json([
+                    'error' => "E-mail ya registrado anteriormente"
+                ]);
+            }
+
+        }
+
+    }
+
+    public function updateProfile(Request $request){
+        
+        $id = $request->input('id');
+        $email = $request->input('email');
+        $name = $request->input('name');
+
+
+        // try {
+        //     return User::where('id', '=', $id)
+        //     ->update(['email' => $email, 'name' =>$name, 'gender' =>$gender, 'orientation' =>$orientation,
+        //     'status' =>$status, 'intention' =>$intention, 'age' =>$age, 'surname' =>$surname]);
+        // } catch (\Throwable $th) {
+        //     //throw $th;
+        // }
+        
+    }
+
+    public function deleteUser(Request $request){
+
+        $id = $request->input('id');
+
+        try {
+            //BUSCA EL PLAYER POR ID. SI EXISTE, BORRA EL PLAYER. SI NO, SACA MENSAJE DE ERROR
+            $arrayUser = USER::all()
+            ->where('id', '=', $id);
+
+            $USER = USER::where('id', '=', $id);
+            
+            if (count($arrayUser) == 0) {
+                return response()->json([
+                    "data" => $arrayUser,
+                    "message" => "No se ha encontrado el USER"
+                ]);
+            }else{
+                $USER->delete();
+                return response()->json([
+                    "data" => $arrayUser,
+                    "message" => "USER borrado correctamente"
+                ]);
+            }
+
+        } catch (QueryException $error) {
+
+            $codigoError = $error->errorInfo[1];
+            if($codigoError){
+                return "Error $codigoError";
+            }
+        }
+    }
     
 }
