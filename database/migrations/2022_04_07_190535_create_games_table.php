5008c27bfa08430c9d80bcba6141f01365fa8b58
Rodrigo <infohotsmoke@gmail.com>
2022-04-07 21:13:25 +0200

    CreateGAmesTable updated

--- database/migrations/2022_04_07_190535_create_games_table.php
+++ database/migrations/2022_04_07_190535_create_games_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+class CreateGamesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -14,7 +14,9 @@ return new class extends Migration
     public function up()
     {
         Schema::create('games', function (Blueprint $table) {
-            $table->id();
+            $table->bigIncrements('id');
+            $table->string('title');
+            $table->string('url');
             $table->timestamps();
         });
     }
