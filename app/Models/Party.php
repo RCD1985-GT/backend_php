e25dcd0f71538fa7f547eb85b519d3605db3c10f
Rodrigo <infohotsmoke@gmail.com>
2022-04-07 20:39:38 +0200

    Party model updated

--- app/Models/Party.php
+++ app/Models/Party.php
@@ -8,4 +8,17 @@ use Illuminate\Database\Eloquent\Model;
 class Party extends Model
 {
     use HasFactory;
+
+    protected $table = 'partys';
+
+    protected $fillable = [
+        'name', 'gameId'
+    ];
+
+    public function messages() {
+
+        return $this->hasMany('App\Models\Message');
+
+    }
+
 }
