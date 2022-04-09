0ca18250eff06296a9ca4534957268bf57d740fb
Rodrigo <infohotsmoke@gmail.com>
2022-04-09 20:02:05 +0200

    foreign key game_id added in parties

--- database/migrations/2022_04_09_105250_create_parties_table.php
+++ database/migrations/2022_04_09_105250_create_parties_table.php
@@ -15,10 +15,12 @@ return new class extends Migration
     {
         Schema::create('parties', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->unsignedBigInteger('game_id');
             $table->timestamps();
 
             // FALTAN FOREIGN KEYS PARA RELACIONAR
-
+            $table->foreign('game_id')->references('id')->on('games');
         });
     }
 
